1b10932107401718442d971d27cd3f6aa591aaff
Gabriel Toll Stålbom <gabtost@gmail.com>
2023-05-16 12:21:39 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,118 @@
+const fs = require("fs");
+const { Client, IntentsBitField } = require("discord.js");
+
+// Discord bot client
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
+});
+
+const state = {
+  isWorking: false,
+  lastUpdateMs: new Date().getTime(),
+};
+
+function writeStateCache() {
+  fs.writeFile("./cache.json", JSON.stringify(state), (err) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+  });
+}
+
+// read cached state to preserve state between restarts
+fs.readFile("./cache.json", (err, data) => {
+  if (err) {
+    // first time setup, cache doesn't exist yet
+    writeStateCache();
+    return;
+  }
+
+  const cachedState = JSON.parse(data);
+  console.log("found cached state", cachedState);
+  state.isWorking = cachedState.isWorking;
+  state.lastUpdateMs = cachedState.lastUpdateMs;
+});
+
+// timeout to prevent username change rate limit
+let timeout = null;
+
+function updateClientUserName() {
+  if (timeout) clearTimeout(timeout);
+  timeout = setTimeout(() => {
+    const username = `Robin ${state.isWorking ? "" : "not "}working`;
+    client.user.setUsername(username).catch((error) => console.error(error));
+  }, 1000 * 60);
+}
+
+function setIsRobinWorkingToday(bool) {
+  state.isWorking = bool;
+  updateClientUserName();
+  writeStateCache();
+}
+
+// checks if date has changed and toggles working state
+function update() {
+  console.log("running update!");
+
+  const lastUpdateDate = new Date(state.lastUpdateMs).getDate();
+  const today = new Date();
+  const date = today.getDate();
+
+  if (date === lastUpdateDate) return;
+
+  state.lastUpdateMs = today.getTime();
+  const day = today.getDay();
+  const isWeekend = day == 6 || day == 0;
+
+  if (isWeekend) return;
+
+  setIsRobinWorkingToday(!isWorking);
+}
+
+// interval to automatically toggle if robin is working next work day
+setInterval(update, 1000 * 60 * 60);
+
+// message content to listen to
+const names = ["robin", "<:pizzarobin:1024343299487698974>"];
+const matches = names.map((name) => `is ${name} working?`);
+
+client.on("messageCreate", async (event, listener) => {
+  const content = event.content.toLowerCase();
+
+  try {
+    if (matches.includes(content)) {
+      await event.reply(state.isWorking ? "yes" : "no");
+      return;
+    }
+
+    // TODO: set up proper slash command
+    if (content === "/is-robin-working no") {
+      setIsRobinWorkingToday(false);
+      await event.reply("ok, robin is not working today");
+      return;
+    }
+
+    // TODO: set up proper slash command
+    if (content === "/is-robin-working yes") {
+      setIsRobinWorkingToday(true);
+      await event.reply("ok, robin is working today");
+      return;
+    }
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+client.on("ready", () => {
+  update();
+  updateClientUserName();
+  console.log("bot online!");
+});
+
+client.login(process.env.TOKEN);
