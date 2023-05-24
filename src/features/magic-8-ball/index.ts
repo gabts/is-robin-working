4e517d8e213b4ff8b32b568701bd4d0d701dd489
Robin Swenson <robin.swenson@ist.com>
2023-05-24 10:11:45 +0200

    Removed some boring answers from magic 8 ball.

--- src/features/magic-8-ball/index.ts
+++ src/features/magic-8-ball/index.ts
@@ -15,12 +15,6 @@ const fortunes = [
   "Yes.",
   "Signs point to yes.",
 
-  "Reply hazy, try again.",
-  "Ask again later.",
-  "Better not tell you now.",
-  "Cannot predict now.",
-  "Concentrate and ask again.",
-
   "Don't count on it.",
   "My reply is no.",
   "My sources say no.",
